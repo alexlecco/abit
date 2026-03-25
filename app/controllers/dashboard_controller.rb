e9aacc01c805058be75e4bbdd55a0cefcc6494a5
Alex Villecco <alexlecco24@gmail.com>
2026-03-25 03:22:21 -0300

    Only show days from earliest habit start date, dim cells before habit creation

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
   def index
     @habits = current_user.habits.active.ordered
-    @days = (Date.current - 29.days..Date.current).to_a.reverse
+    earliest = @habits.minimum(:start_date) || Date.current
+    start = [ earliest, Date.current - 29.days ].max
+    @days = (start..Date.current).to_a.reverse
 
     @completions = current_user.habit_logs
                                .where(completed_on: @days)
